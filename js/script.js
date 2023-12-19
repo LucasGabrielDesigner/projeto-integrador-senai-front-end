620a5cd18fa782a812cb09fbc2aff6f618b9b9b2
Lucas Coelho Gabriel <139984013+LucasGabrielDesigner@users.noreply.github.com>
2023-12-19 02:09:16 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function submitForm() {
+    // Simulando o envio do formulário
+    // Exibindo a mensagem de agradecimento
+    // var name = document.gatElementById("name").value;
+    // name.innerHTML= `<p>Obrigado, ${name} por entrar em contato! Entrarei em contato o mais rápido possível!</p>`
+
+    var contato = document.getElementById("contactForm").style.display = 'none';
+    var obrigado = document.getElementById("thankYouMessage").style.display = 'block';
+}
